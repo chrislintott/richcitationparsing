79e553a819f91e54ba208d3176e1b4598d1be1db
chrislintott <cjl@astro.ox.ac.uk>
2014-12-09 12:30:58 -0600

    read in and print

--- import.py
+++ import.py
@@ -2,5 +2,11 @@ import json
 from pprint import pprint
 json_data=open('./richcitationexample.json')
 jdata = json.load(json_data)
-pprint (jdata)
-json_data.close()
+# pprint (jdata)
+
+# titles = [element["title"] for element in jdata]
+print "url is "+ str(jdata[0]["bibliographic"]["subject"])
+text = [group["context"]["text_before"] for group in jdata[0]["citation_groups"]]
+print text
+
+#pprint(titles)
